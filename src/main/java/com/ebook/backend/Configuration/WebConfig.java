8f59e1cf1eb0ede3c235b8f8b6b656dcb2e32074
fly0307 <fly0307@sjtu.edu.cn>
2022-11-15 23:58:47 +0800

    后端接收请求无法返回前端

--- src/main/java/com/ebook/backend/Configuration/WebConfig.java
+++ src/main/java/com/ebook/backend/Configuration/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOriginPattern("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setAllowCredentials(true);
